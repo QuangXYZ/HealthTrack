5dd6114c6681e1128fa6a8821224513096856bf4
Nguyen Quang <thanhquang.lord@gmail.com>
2024-03-10 19:15:52 +0700

    add CreateChallengeActivity

--- app/src/main/java/com/example/healthtrack/Views/Fragment/PrivateChallengeFragment.java
+++ app/src/main/java/com/example/healthtrack/Views/Fragment/PrivateChallengeFragment.java
@@ -1,6 +1,8 @@
 package com.example.healthtrack.Views.Fragment;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -10,9 +12,12 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.healthtrack.R;
 import com.example.healthtrack.Views.Adapters.PrivateChallengeAdapter;
+import com.example.healthtrack.Views.CreateChallengeActivity;
+import com.example.healthtrack.Views.PrivateChallengeDetail;
 
 import java.util.ArrayList;
 
@@ -20,6 +25,7 @@ public class PrivateChallengeFragment extends Fragment {
     RecyclerView challengeRecyclerview;
     PrivateChallengeAdapter adapter;
     ArrayList<Integer> challenges;
+    Button createChallengeBtn;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -27,11 +33,13 @@ public class PrivateChallengeFragment extends Fragment {
         // Inflate the layout for this fragment
         View view =  inflater.inflate(R.layout.fragment_private_challenge, container, false);
         init(view);
+        settingUpListener();
         return view;
     }
 
     void init(View view) {
         challengeRecyclerview = view.findViewById(R.id.private_challenge_recyclerview);
+        createChallengeBtn = view.findViewById(R.id.fragment_challenge_create_button);
         challenges = new ArrayList<>();
         challenges.add(1);
         challenges.add(2);
@@ -42,6 +50,17 @@ public class PrivateChallengeFragment extends Fragment {
         challengeRecyclerview.setAdapter(adapter);
         challengeRecyclerview.setNestedScrollingEnabled(true);
 
+    }
+    void settingUpListener() {
+        createChallengeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context = getContext();
+                Intent intent = new Intent(context, CreateChallengeActivity.class);
+                startActivity(intent);
+                getActivity().overridePendingTransition(R.anim.slide_in_from_right, R.anim.slide_out_to_left);
+            }
+        });
     }
 
 }
