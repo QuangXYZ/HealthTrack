5a22f6e5655804121fb03514bf8f815e5a481fa4
LAPTOP-38ED4DO3\ASUS <nhuy.danghuynh2002@gmail.com>
2024-03-10 17:42:14 +0700

    Thiết kế giao diện HistoryStepActivity

---
+++ app/src/main/java/com/example/healthtrack/Views/Activity/SetGoalsActivity.java
@@ -0,0 +1,16 @@
+package com.example.healthtrack.Views.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.healthtrack.R;
+
+public class SetGoalsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_goals);
+    }
+}
