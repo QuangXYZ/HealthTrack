64fd509a4dc7814ce1dd4f2f5b27ca97ddba7b2e
Nguyen Quang <thanhquang.lord@gmail.com>
2024-03-10 18:46:19 +0700

    update detail challenge

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,9 @@ dependencies {
     testImplementation ("junit:junit:4.+")
     androidTestImplementation ("androidx.test.ext:junit:1.1.5")
     androidTestImplementation ("androidx.test.espresso:espresso-core:3.5.1")
+    implementation ("com.journeyapps:zxing-android-embedded:4.1.0")
+    implementation ("com.google.zxing:core:3.3.3")
+
 
 
 }
