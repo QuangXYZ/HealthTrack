7c17c56a5d3920d3f0c3a32f6d43b881c8f02f3e
Doan-Toan <96815799+Doan-Toan@users.noreply.github.com>
2024-03-13 16:54:00 +0700

    update FriendsAdapter, delete Profile Activity, delete FriendsViewHolder

--- app/src/main/java/com/example/healthtrack/Views/Adapters/FriendsAdapter.java
+++ app/src/main/java/com/example/healthtrack/Views/Adapters/FriendsAdapter.java
@@ -2,7 +2,10 @@ package com.example.healthtrack.Views.Adapters;
 
 import android.content.Context;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -12,7 +15,7 @@ import com.example.healthtrack.R;
 
 import java.util.List;
 
-public class FriendsAdapter extends RecyclerView.Adapter<FriendViewHolder> {
+public class FriendsAdapter extends RecyclerView.Adapter<FriendsAdapter.FriendViewHolder> {
     Context context;
     List<Friends> friends;
 
@@ -36,5 +39,16 @@ public class FriendsAdapter extends RecyclerView.Adapter<FriendViewHolder> {
     @Override
     public int getItemCount() {
         return friends.size();
+    }
+
+    public class FriendViewHolder extends RecyclerView.ViewHolder {
+        ImageView friendAva;
+        TextView name;
+
+        public FriendViewHolder(@NonNull View itemView) {
+            super(itemView);
+            friendAva = itemView.findViewById(R.id.friendAva);
+            name = itemView.findViewById(R.id.name);
+        }
     }
 }
