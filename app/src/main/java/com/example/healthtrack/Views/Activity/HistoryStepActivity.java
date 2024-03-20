dcd40b9a6ffe204301ce3a0cea5169d3fbcf53ac
LAPTOP-38ED4DO3\ASUS <nhuy.danghuynh2002@gmail.com>
2024-03-20 20:55:50 +0700

    update get api step display step information

--- app/src/main/java/com/example/healthtrack/Views/Activity/HistoryStepActivity.java
+++ app/src/main/java/com/example/healthtrack/Views/Activity/HistoryStepActivity.java
@@ -8,7 +8,6 @@ import android.app.Dialog;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Gravity;
@@ -28,6 +27,7 @@ import com.example.healthtrack.Respone.SetGoalsResponse;
 import com.example.healthtrack.Respone.StepResponse;
 import com.example.healthtrack.SharedPreferences.SharedPrefUser;
 import com.google.android.material.progressindicator.CircularProgressIndicator;
+import com.google.android.material.progressindicator.LinearProgressIndicator;
 import com.squareup.timessquare.CalendarPickerView;
 
 import java.text.SimpleDateFormat;
@@ -41,7 +41,8 @@ public class HistoryStepActivity extends AppCompatActivity {
     private SetGoalsController setGoalsController;
     private ArrayList<Step> mListStep;
     private ArrayList<SetGoals> mListSetGoals;
-    private int step2, stepGoals;
+    private int step1, calo, stepGoals, caloGoals, kmGoals, km;
+    private int time, timeGoals;
 
 
     @Override
@@ -92,15 +93,14 @@ public class HistoryStepActivity extends AppCompatActivity {
                 // Hiển thị ngày được chọn
                 SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
                 String formattedDate = sdf.format(date);
-
                 //Hiển thị ngày lên dialog
-                SimpleDateFormat sdf2 = new SimpleDateFormat("dd-MM-yyyy");
+                SimpleDateFormat sdf2 = new SimpleDateFormat("dd/MM/yyyy");
                 String formattedDate2 = sdf2.format(date);
-
-                Toast.makeText(getApplicationContext(), "Ngày được chọn: " + formattedDate, Toast.LENGTH_SHORT).show();
-                Log.d(TAG, "Selected time in millis: " + formattedDate);
-
-                openHistoryStepDialog(Gravity.BOTTOM, formattedDate, formattedDate2);
+                try {
+                    openHistoryStepDialog(Gravity.BOTTOM, formattedDate, formattedDate2);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
             }
 
             @Override
@@ -110,18 +110,19 @@ public class HistoryStepActivity extends AppCompatActivity {
         });
     }
 
-    private void openHistoryStepDialog(int gravity, String date, String dateHistory) {
+    private void openHistoryStepDialog(int gravity, String date, String dateHistory) throws InterruptedException {
         final Dialog dialog = new Dialog(this);
         dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
         dialog.setContentView(R.layout.layout_dialog_history_step);
 
         TextView tvStep, tvCalo, tvTime, tvKm, tvStepGoals, tvCaloGoals, tvTimeGoals, tvKmGoals, tvDateHistory;
-        CircularProgressIndicator progressStep, progressCalo, progressTime, progressKm;
+        CircularProgressIndicator progressStep;
+        LinearProgressIndicator progressCalo, progressTime, progressKm;
 
         progressStep = (CircularProgressIndicator) dialog.findViewById(R.id.circularProgressIndicator_history_step);
-//        progressCalo = (CircularProgressIndicator) dialog.findViewById(R.id.circularProgressIndicator_history_calo);
-//        progressTime = (CircularProgressIndicator) dialog.findViewById(R.id.circularProgressIndicator_history_time);
-//        progressKm = (CircularProgressIndicator) dialog.findViewById(R.id.circularProgressIndicator_history_km);
+        progressCalo = (LinearProgressIndicator) dialog.findViewById(R.id.circularProgressIndicator_history_calo);
+        progressTime = (LinearProgressIndicator) dialog.findViewById(R.id.circularProgressIndicator_history_time);
+        progressKm = (LinearProgressIndicator) dialog.findViewById(R.id.circularProgressIndicator_history_km);
         tvDateHistory = dialog.findViewById(R.id.date_history);
         tvStep = dialog.findViewById(R.id.step_dialog_history);
         tvCalo = dialog.findViewById(R.id.calo_dialog_history);
@@ -132,10 +133,9 @@ public class HistoryStepActivity extends AppCompatActivity {
         tvTimeGoals = dialog.findViewById(R.id.time_goals_dialog_history);
         tvKmGoals = dialog.findViewById(R.id.km_goals_dialog_history);
 
-        tvDateHistory.setText(dateHistory);
+        tvDateHistory.setText("Ngày " + dateHistory);
 
         String idUser = SharedPrefUser.getId(HistoryStepActivity.this);
-
         stepController.getStepHistory(HistoryStepActivity.this, idUser, date, new StepController.GetHistoryCallback() {
             @Override
             public void onSuccess(StepResponse<Step> step) {
@@ -145,10 +145,15 @@ public class HistoryStepActivity extends AppCompatActivity {
                     tvCalo.setText(String.valueOf(mListStep.get(0).getCalo()));
                     tvTime.setText(String.valueOf(mListStep.get(0).getTime()));
                     tvKm.setText(String.valueOf(mListStep.get(0).getDistance()));
-                    step2 = mListStep.get(0).getNumberStep();
-                    Log.d(TAG, "Phần trăm số bước chân step2: " + step2);
-                    Log.d(TAG, "Phần trăm số bước chân mListStep: " + mListStep.get(0).getNumberStep());
+                    step1 = mListStep.get(0).getNumberStep();
+                    calo = mListStep.get(0).getCalo();
+                    time = Integer.valueOf(mListStep.get(0).getTime());
+                    km = Integer.valueOf((int) mListStep.get(0).getDistance());
                 }
+                calculateProgressStep(progressStep);
+                calculateProgressCalo(progressCalo);
+                calculateProgressTime(progressTime);
+                calculateProgressDistance(progressKm);
             }
 
             @Override
@@ -167,7 +172,14 @@ public class HistoryStepActivity extends AppCompatActivity {
                     tvCaloGoals.setText(String.valueOf("/" + mListSetGoals.get(0).getCaloGoals()) + "kcal");
                     tvTimeGoals.setText(String.valueOf("/" + mListSetGoals.get(0).getTimeGoals()) + "phút");
                     stepGoals = mListSetGoals.get(0).getNumberStepGoals();
+                    kmGoals = mListSetGoals.get(0).getDistanceGoals();
+                    caloGoals = mListSetGoals.get(0).getCaloGoals();
+                    timeGoals = Integer.valueOf(mListSetGoals.get(0).getCaloGoals());
                 }
+                calculateProgressStep(progressStep);
+                calculateProgressDistance(progressKm);
+                calculateProgressCalo(progressCalo);
+                calculateProgressTime(progressTime);
             }
 
             @Override
@@ -175,10 +187,6 @@ public class HistoryStepActivity extends AppCompatActivity {
                 Toast.makeText(HistoryStepActivity.this, "Lỗi kết nô api", Toast.LENGTH_SHORT).show();
             }
         });
-
-        Log.d(TAG, "Phần trăm số bước chân1: " + step2);
-        Log.d(TAG, "Phần trăm số bước chân2: " + stepGoals);
-//                progressStep.setProgress(Math.round(step2 * 100 / stepGoals));
 
         Window window = dialog.getWindow();
         if (window == null) {
@@ -193,6 +201,14 @@ public class HistoryStepActivity extends AppCompatActivity {
 
         //xử lý khi click ra ngoài dialog
         if (Gravity.BOTTOM == gravity) {
+            step1 = 0;
+            stepGoals = 0;
+            calo = 0;
+            caloGoals = 0;
+            km = 0;
+            kmGoals = 0;
+            time = 0;
+            timeGoals = 0;
             dialog.setCancelable(true);
         } else {
             dialog.setCancelable(false);
@@ -202,6 +218,14 @@ public class HistoryStepActivity extends AppCompatActivity {
         canCelDialog.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                step1 = 0;
+                stepGoals = 0;
+                calo = 0;
+                caloGoals = 0;
+                km = 0;
+                kmGoals = 0;
+                time = 0;
+                timeGoals = 0;
                 dialog.dismiss();
             }
         });
@@ -209,9 +233,30 @@ public class HistoryStepActivity extends AppCompatActivity {
         dialog.show();
     }
 
-    private int calculatePercentage(int number1, int number2) {
-        return Math.round(number1 * 100 / number2);
+    private void calculateProgressStep(CircularProgressIndicator progressStep) {
+        if (step1 != 0 && stepGoals != 0) {
+            progressStep.setProgress(Math.round(step1 * 100 / stepGoals));
+        }
     }
+
+    private void calculateProgressCalo(LinearProgressIndicator progressCalo) {
+        if (calo != 0 && caloGoals != 0) {
+            progressCalo.setProgress(Math.round(calo * 100 / caloGoals));
+        }
+    }
+
+    private void calculateProgressTime(LinearProgressIndicator progressTime) {
+        if (time != 0 && timeGoals != 0) {
+            progressTime.setProgress(Integer.valueOf(Math.round(time * 100 / timeGoals)));
+        }
+    }
+
+    private void calculateProgressDistance(LinearProgressIndicator progressDistance) {
+        if (km != 0 && kmGoals != 0) {
+            progressDistance.setProgress(Integer.valueOf((int) Math.round(km * 100 / kmGoals)));
+        }
+    }
+
 
     private Float calculatePercentageFloat(Float number1, Float number2) {
         return (float) Math.round(number1 * 100 / number2);
@@ -223,66 +268,3 @@ public class HistoryStepActivity extends AppCompatActivity {
 //                .withSelectedDate(new Date());
 }
 
-/*class MyAsyncTask extends AsyncTask<Void, Void, Void> {
-            // Thực hiện tác vụ cần đợi
-            @Override
-            protected Void doInBackground(Void... params) {
-                stepController.getStepHistory(HistoryStepActivity.this, idUser, date, new StepController.GetHistoryCallback() {
-                    @Override
-                    public void onSuccess(StepResponse<Step> step) {
-                        mListStep = (ArrayList<Step>) step.getData();
-                        for (int i = 0; i < mListStep.size(); i++) {
-                            tvStep.setText(String.valueOf(mListStep.get(0).getNumberStep()));
-                            tvCalo.setText(String.valueOf(mListStep.get(0).getCalo()));
-                            tvTime.setText(String.valueOf(mListStep.get(0).getTime()));
-                            tvKm.setText(String.valueOf(mListStep.get(0).getDistance()));
-                            step2 = mListStep.get(0).getNumberStep();
-                            Log.d(TAG, "Phần trăm số bước chân step2: " + step2);
-                            Log.d(TAG, "Phần trăm số bước chân mListStep: " + mListStep.get(0).getNumberStep());
-                        }
-                    }
-
-                    @Override
-                    public void onFailure() {
-                        Toast.makeText(HistoryStepActivity.this, "Lỗi kết nô api", Toast.LENGTH_SHORT).show();
-                    }
-                });
-
-                setGoalsController.getSetGoals(HistoryStepActivity.this, idUser, new SetGoalsController.Callback() {
-                    @Override
-                    public void onSetGoals(SetGoalsResponse<SetGoals> setGoals) {
-                        mListSetGoals = (ArrayList<SetGoals>) setGoals.getData();
-                        for (int i = 0; i < mListSetGoals.size(); i++) {
-                            tvStepGoals.setText(String.valueOf("/" + mListSetGoals.get(0).getNumberStepGoals()) + "bước");
-                            tvKmGoals.setText(String.valueOf("/" + mListSetGoals.get(0).getDistanceGoals()) + "km");
-                            tvCaloGoals.setText(String.valueOf("/" + mListSetGoals.get(0).getCaloGoals()) + "kcal");
-                            tvTimeGoals.setText(String.valueOf("/" + mListSetGoals.get(0).getTimeGoals()) + "phút");
-                            stepGoals = mListSetGoals.get(0).getNumberStepGoals();
-                        }
-                    }
-
-                    @Override
-                    public void onFailure() {
-                        Toast.makeText(HistoryStepActivity.this, "Lỗi kết nô api", Toast.LENGTH_SHORT).show();
-                    }
-                });
-                return null;
-            }
-
-            @Override
-            protected void onPostExecute(Void result) {
-                // Thực hiện các hành động sau khi tác vụ hoàn thành
-                try {
-                    Thread.sleep(1000);
-                    Log.d(TAG, "Phần trăm số bước chân1: " + step2);
-                    Log.d(TAG, "Phần trăm số bước chân2: " + stepGoals);
-                } catch (Exception e){
-
-                }
-//                progressStep.setProgress(Math.round(step2 * 100 / stepGoals));
-            }
-        }
-
-        // Khởi tạo và thực thi AsyncTask
-        MyAsyncTask task = new MyAsyncTask();
-        task.execute();*/
